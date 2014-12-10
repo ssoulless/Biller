cc1d8bae7e907ea2e96c7ba00f47475f7abcaebe
Sebastian Velandia <sebas.velandia27@gmail.com>
2014-12-09 22:37:22 -0500

    Added employees controller and view with creation form

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,21 @@
 class EmployeesController < ApplicationController
+	def index
+		@employees = Employee.all
+	end
+
+	def show
+		@employee = Employee.find(params[:id])
+	end
+
+	def create
+		@employee = Employee.new(params.require(:employee).permit(:name))
+		if @employee.save
+			flash[:success] = %Q|Added employee|
+			redirect_to accounts_path
+		else
+			flash[:error] = %Q|There was a problem adding employee|
+			render action: :new
+		end
+	end
 end
+	
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,4 @@
 class Account < ActiveRecord::Base
 	has_many :account_entries
+	validates :name, presence: true
 end
