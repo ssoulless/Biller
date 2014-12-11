2b28a1c192ca01f8a2b541fece7a85bd04578d92
Sebastian Velandia <sebas.velandia27@gmail.com>
2014-12-11 14:28:53 -0500

    Added basic validation to models

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,7 @@
 class Account < ActiveRecord::Base
 	has_many :account_entries
-	validates :name, presence: true
+
+	validates :name, presence: true, 
+					 length: {in: 1..70, message: %Q|Please enter decent name Sr.|},
+					 uniqueness: true
 end
